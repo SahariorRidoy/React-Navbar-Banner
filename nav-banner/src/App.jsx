c8e5bfaebf79b180ba9e87a9bef072c23a10d38c
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-10-28 22:58:00 +0600

    Setup react with tailwind css

---
+++ nav-banner/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav className='flex justify-between'>
+        <h1 className='font-bold text-4xl'>Sample</h1>
+        <ul className='flex gap-3 font-semibold'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
